8ba244ae4dead896f90d74428006b033a40291ad
arapt <arapt@DESKTOP-70PT2F0>
2023-05-04 20:04:44 -0400

    Add project files.

---
+++ MauiNotes/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiNotes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+        Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+    }
+}
---
+++ MauiNotes/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiNotes.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+            await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
